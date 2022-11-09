a7da1cc48ad62018a91dcc34d50aec514ba77e61
MustafaAdel <mustafaadelsoftwaredeveloper@gmail.com>
2022-11-09 03:56:17 +0200

    this server app using typescript express framework

---
+++ index.ts
@@ -0,0 +1,28 @@
+import express, { RequestHandler } from "express";
+import { isNamedExportBindings } from "typescript";
+import { db } from "./datastore";
+
+const app = express();
+
+app.use(express.json());
+
+// const posts: any[] = [];
+
+const requestLoggerMiddleware: RequestHandler = (req, res, next) => {
+  console.log(req.method, req.path, "- body:", req.body);
+  next();
+};
+
+app.use(requestLoggerMiddleware);
+
+app.get("/posts", (request, response) => {
+  response.send({ posts: db.listPost() });
+});
+
+app.post("/posts", (request, response) => {
+  const post = request.body;
+  db.createPost(post);
+  response.sendStatus(200);
+});
+
+app.listen(3000);
